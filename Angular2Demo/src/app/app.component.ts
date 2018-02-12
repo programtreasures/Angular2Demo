085a9e2909b446448a80873421e1c2b36119d029
techtrainer55 <technical.trainer7@gmail.com>
2018-02-12 10:41:11 +0530

    Add project files.

---
+++ Angular2Demo/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({ 
+    selector: 'my-app',
+    template: `<h1>{{name}}<h1>`
+}) 
+export class AppComponent{
+    name:string = "Test fdas"
+}
