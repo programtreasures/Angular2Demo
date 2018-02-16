9e3bbeb1fd18df54e599aae224068482ba50831e
techtrainer55 <technical.trainer7@gmail.com>
2018-02-16 10:24:34 +0530

    trackby for ngFor, filters

--- Angular2Demo/src/app/employee/employeelist.component.js
+++ Angular2Demo/src/app/employee/employeelist.component.js
@@ -5,17 +5,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var EmployeeListComponent = (function () {
     function EmployeeListComponent() {
         this.employees = [
-            { code: 'emp001', name: 'Test1', emailId: 'test1@gmail.com', dateofjoining: '05/01/2011' },
-            { code: 'emp002', name: 'Test2', emailId: 'test2@gmail.com', dateofjoining: '10/12/2011' },
-            { code: 'emp003', name: 'Test3', emailId: 'test3@gmail.com', dateofjoining: '20/04/2015' },
-            { code: 'emp004', name: 'Test4', emailId: 'test4@gmail.com', dateofjoining: '22/01/2010' }
+            { code: 'emp001', name: 'Test1', emailId: 'test1@gmail.com', salary: 5500.50, dateofjoining: '05/01/2011' },
+            { code: 'emp002', name: 'Test2', emailId: 'test2@gmail.com', salary: 5540.00, dateofjoining: '10/12/2011' },
+            { code: 'emp003', name: 'Test3', emailId: 'test3@gmail.com', salary: 6500.00, dateofjoining: '04/24/2015' },
+            { code: 'emp004', name: 'Test4', emailId: 'test4@gmail.com', salary: 5500.00, dateofjoining: '01/11/2010' }
         ];
     }
+    EmployeeListComponent.prototype.trackByEmployees = function (index, empoyee) {
+        return empoyee.code;
+    };
+    EmployeeListComponent.prototype.refreshEmployees = function () {
+        this.employees = [
+            { code: 'emp001', name: 'Test1', emailId: 'test1@gmail.com', salary: 5500.50, dateofjoining: '05/01/2011' },
+            { code: 'emp002', name: 'Test2', emailId: 'test2@gmail.com', salary: 5540.00, dateofjoining: '10/12/2011' },
+            { code: 'emp003', name: 'Test3', emailId: 'test3@gmail.com', salary: 6500.00, dateofjoining: '04/24/2015' },
+            { code: 'emp004', name: 'Test4', emailId: 'test4@gmail.com', salary: 5500.00, dateofjoining: '01/11/2010' },
+            { code: 'emp005', name: 'Test5', emailId: 'test5@gmail.com', salary: 5000.00, dateofjoining: '03/14/2014' }
+        ];
+    };
     return EmployeeListComponent;
 }());
 EmployeeListComponent = __decorate([
@@ -26,7 +41,8 @@ EmployeeListComponent = __decorate([
         styles: ["table {\n                color: #369;\n                font-family: Arial, Helvetica, sans-serif;\n                font-size: large;\n                border: 1px solid black;\n            }",
             "td {\n                    border: 1px solid red;\n                }"
         ],
-    })
+    }),
+    __metadata("design:paramtypes", [])
 ], EmployeeListComponent);
 exports.EmployeeListComponent = EmployeeListComponent;
 //# sourceMappingURL=employeelist.component.js.map
--- Angular2Demo/src/app/employee/employeelist.component.ts
+++ Angular2Demo/src/app/employee/employeelist.component.ts
@@ -15,12 +15,30 @@
                 }`
     ],
 })
-export class EmployeeListComponent
-{
-    employees: any[] = [
-        { code: 'emp001', name: 'Test1', emailId: 'test1@gmail.com', dateofjoining: '05/01/2011' },
-        { code: 'emp002', name: 'Test2', emailId: 'test2@gmail.com', dateofjoining: '10/12/2011' },
-        { code: 'emp003', name: 'Test3', emailId: 'test3@gmail.com', dateofjoining: '20/04/2015' },
-        { code: 'emp004', name: 'Test4', emailId: 'test4@gmail.com', dateofjoining: '22/01/2010' }
-    ];
+export class EmployeeListComponent {
+    employees: any[]
+
+    constructor() {
+        this.employees = [
+            { code: 'emp001', name: 'Test1', emailId: 'test1@gmail.com', salary: 5500.50, dateofjoining: '05/01/2011' },
+            { code: 'emp002', name: 'Test2', emailId: 'test2@gmail.com', salary: 5540.00, dateofjoining: '10/12/2011' },
+            { code: 'emp003', name: 'Test3', emailId: 'test3@gmail.com', salary: 6500.00, dateofjoining: '04/24/2015' },
+            { code: 'emp004', name: 'Test4', emailId: 'test4@gmail.com', salary: 5500.00, dateofjoining: '01/11/2010' }
+        ];
+    }
+
+    trackByEmployees(index: number, empoyee: any) {
+        return empoyee.code;
+    }
+
+    refreshEmployees() {
+        this.employees = [
+            { code: 'emp001', name: 'Test1', emailId: 'test1@gmail.com', salary: 5500.50, dateofjoining: '05/01/2011' },
+            { code: 'emp002', name: 'Test2', emailId: 'test2@gmail.com', salary: 5540.00, dateofjoining: '10/12/2011' },
+            { code: 'emp003', name: 'Test3', emailId: 'test3@gmail.com', salary: 6500.00, dateofjoining: '04/24/2015' },
+            { code: 'emp004', name: 'Test4', emailId: 'test4@gmail.com', salary: 5500.00, dateofjoining: '01/11/2010' },
+            { code: 'emp005', name: 'Test5', emailId: 'test5@gmail.com', salary: 5000.00, dateofjoining: '03/14/2014' }
+        ];
+    }
+
 }
